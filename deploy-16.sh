b050cec484e867ef52089fbf15c49e74555f5bfb
kukupigs <kukupigs@sina.com>
2017-02-28 22:35:36 +0800

    增加两个在新建l所需的扩展

--- deploy-16.sh
+++ deploy-16.sh
@@ -72,7 +72,8 @@ apt-get install -y --force-yes php7.0-cli php7.0 \
 php-pgsql php-sqlite3 php-gd php-apcu \
 php-curl php7.0-mcrypt \
 php-imap php-mysql php-memcached php7.0-readline php-xdebug \
-php-mbstring php-xml php7.0-zip php7.0-intl php7.0-bcmath php-soap
+php-mbstring php-xml php7.0-zip php7.0-mbstring php7.0-dom \
+php7.0-soap php7.0-intl php7.0-bcmath php-soap
 
 # Install Composer
 
