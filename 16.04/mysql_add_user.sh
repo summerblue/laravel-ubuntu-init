59e7707e2b304f510e0ff7096947472557e15f9a
leo108 <leo108@qq.com>
2018-08-27 00:12:38 +0800

    bug fix

--- 16.04/mysql_add_user.sh
+++ 16.04/mysql_add_user.sh
@@ -37,9 +37,9 @@ ansi --green --bold "用户名："; ansi -n --bg-yellow --black ${MYSQL_NORMAL_U
 ansi --green --bold "密码："; ansi -n --bg-yellow --black ${MYSQL_NORMAL_USER_PASSWORD}
 
 if [[ CREATE_DB -eq 1 ]]; then
-    DATABASE_NAME=${MYSQL_NORMAL_USER/\-/_}
-    mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE ${DATABASE_NAME};" >> ${LOG_PATH} 2>&1
-    mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON ${DATABASE_NAME}.* TO '${MYSQL_NORMAL_USER}';" >> ${LOG_PATH} 2>&1
+    DATABASE_NAME=${MYSQL_NORMAL_USER}
+    mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE '${DATABASE_NAME}';" >> ${LOG_PATH} 2>&1
+    mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON '${DATABASE_NAME}'.* TO '${MYSQL_NORMAL_USER}';" >> ${LOG_PATH} 2>&1
     mysql --user="root" --password="${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;" >> ${LOG_PATH} 2>&1
 fi
 
